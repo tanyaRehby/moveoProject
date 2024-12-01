f895d2ba8900c7234e4c528e7ceb2344e3a076b2
tanyaRehby <tanyarehby@gmail.com>
2024-12-02 01:33:34 +0200

    S

--- server/src/server.js
+++ server/src/server.js
@@ -12,29 +12,26 @@ const rooms = new Map(); // stores the rooms and their associated data
 
 const activeConnections = new Map();
 
-app.use(
-  cors({
-    origin: "https://magenta-cucurucho-e3417f.netlify.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-app.use(express.json());
-app.use("/api/codeblocks", codeBlockRoute);
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 
 const io = new Server(server, {
   allowEIO3: true,
   cors: {
-    origin: "https://magenta-cucurucho-e3417f.netlify.app",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
   },
   pingTimeout: 60000,
   pingInterval: 25000,
 });
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json());
+app.use("/api/codeblocks", codeBlockRoute);
+
 io.on("connection", (socket) => {
   console.log("New user connected:", socket.id);
 
@@ -110,11 +107,9 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 4000; //the port the server will listen
+const PORT = 4000; //the port the server will listen
 mongoose
-  .connect(
-    "mongodb+srv://tanyarehby:bB4p4vC2xeTakzyp@cluster0.79dfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/codeblocks"
-  ) // establishing connection to mongodb
+  .connect("mongodb://localhost:27017/codeblocks") // establishing connection to mongodb
   .then(() => console.log("MongoDB connected successfully")) //success message if mongodb connection is established
   .catch((error) => console.error("MongoDB connection error:", error)); // error if mongodb connection is fails
 
